61940430e447ed721e99a069a1f4205876ca7f13
IHateComputer1 <114846652+IHateComputer1@users.noreply.github.com>
2022-10-29 09:38:09 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,12 +13,33 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onButtonPressed(Button.AB, function () {
     for (let index = 0; index <= 5; index++) {
-        led.plot(index, index % (0 + 1))
+        led.plot(index, index % (index + 1))
         basic.pause(200)
     }
 })
 input.onButtonPressed(Button.B, function () {
-	
+    i_hate_computer = 0
+    i_very_hate_computer = 0
+    for (let index = 0; index < 2; index++) {
+        for (let index = 0; index < 5; index++) {
+            led.plot(i_hate_computer, i_very_hate_computer)
+            basic.pause(200)
+            i_hate_computer += 1
+        }
+        i_very_hate_computer += 1
+        for (let index = 0; index < 6; index++) {
+            led.plot(i_hate_computer, i_very_hate_computer)
+            basic.pause(200)
+            i_hate_computer += -1
+        }
+        i_hate_computer = 0
+        i_very_hate_computer += 1
+    }
+    for (let index = 0; index < 5; index++) {
+        led.plot(i_hate_computer, i_very_hate_computer)
+        basic.pause(200)
+        i_hate_computer += 1
+    }
 })
 let i_very_hate_computer = 0
 let i_hate_computer = 0
